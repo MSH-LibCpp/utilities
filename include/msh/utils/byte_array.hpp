c965f87c5ebbb8bdf972672a51deb2f14a65ee5a
MShojaa <maziar.shojaa@gmail.com>
2025-03-24 07:54:43 +0330

    Update install (remove extra installing) and add insert method to ByteArray

--- include/msh/utils/byte_array.hpp
+++ include/msh/utils/byte_array.hpp
@@ -19,15 +19,15 @@ class ByteArray {
     // Constructors
     ByteArray() = default;
 
-    explicit ByteArray(size_type size, value_type value = 0) : data_(size, value) {}
+    explicit ByteArray(const size_type size, const value_type value = 0) : m_data(size, value) {}
 
-    ByteArray(const value_type* data, size_type size) : data_(data, data + size) {}
+    ByteArray(const value_type* data, const size_type size) : m_data(data, data + size) {}
 
-    ByteArray(std::initializer_list<value_type> init) : data_(init) {}
+    ByteArray(const std::initializer_list<value_type> init) : m_data(init) {}
 
     explicit ByteArray(const std::string& str)
-        : data_(reinterpret_cast<const value_type*>(str.data()),
-                reinterpret_cast<const value_type*>(str.data()) + str.size()) {}
+        : m_data(reinterpret_cast<const value_type*>(str.data()),
+                 reinterpret_cast<const value_type*>(str.data()) + str.size()) {}
 
     // Copy operations
     ByteArray(const ByteArray&) = default;
@@ -38,85 +38,119 @@ class ByteArray {
     ByteArray& operator=(ByteArray&&) noexcept = default;
 
     // Element access
-    value_type& at(size_type pos) {
-        return data_.at(pos);
+    value_type& at(const size_type pos) {
+        return m_data.at(pos);
     }
-    const value_type& at(size_type pos) const {
-        return data_.at(pos);
+    const value_type& at(const size_type pos) const {
+        return m_data.at(pos);
     }
 
-    value_type& operator[](size_type pos) {
-        return data_[pos];
+    value_type& operator[](const size_type pos) {
+        return m_data[pos];
     }
-    const value_type& operator[](size_type pos) const {
-        return data_[pos];
+    const value_type& operator[](const size_type pos) const {
+        return m_data[pos];
+    }
+
+    value_type& back() {
+        if (empty()) {
+            throw std::out_of_range("ByteArray is empty");
+        }
+        return m_data.back();
+    }
+    const value_type& back() const {
+        if (empty()) {
+            throw std::out_of_range("ByteArray is empty");
+        }
+        return m_data.back();
     }
 
     value_type* data() noexcept {
-        return data_.data();
+        return m_data.data();
     }
     const value_type* data() const noexcept {
-        return data_.data();
+        return m_data.data();
     }
 
     // Iterators
     iterator begin() noexcept {
-        return data_.begin();
+        return m_data.begin();
     }
     const_iterator begin() const noexcept {
-        return data_.begin();
+        return m_data.begin();
     }
     const_iterator cbegin() const noexcept {
-        return data_.cbegin();
+        return m_data.cbegin();
     }
 
     iterator end() noexcept {
-        return data_.end();
+        return m_data.end();
     }
     const_iterator end() const noexcept {
-        return data_.end();
+        return m_data.end();
     }
     const_iterator cend() const noexcept {
-        return data_.cend();
+        return m_data.cend();
     }
 
     // Capacity
     bool empty() const noexcept {
-        return data_.empty();
+        return m_data.empty();
     }
     size_type size() const noexcept {
-        return data_.size();
+        return m_data.size();
     }
-    void reserve(size_type new_cap) {
-        data_.reserve(new_cap);
+    void reserve(const size_type new_cap) {
+        m_data.reserve(new_cap);
     }
     size_type capacity() const noexcept {
-        return data_.capacity();
+        return m_data.capacity();
     }
 
     // Modifiers
     void clear() noexcept {
-        data_.clear();
+        m_data.clear();
+    }
+
+    iterator insert(const_iterator pos, const value_type& value) {
+        return m_data.insert(pos, value);
+    }
+
+    iterator insert(const_iterator pos, value_type&& value) {
+        return m_data.insert(pos, std::move(value));
+    }
+
+    iterator insert(const_iterator pos, size_type count, const value_type& value) {
+        return m_data.insert(pos, count, value);
+    }
+
+    template<class InputIt>
+    iterator insert(const_iterator pos, InputIt first, InputIt last) {
+        return m_data.insert(pos, first, last);
+    }
+
+    iterator insert(const_iterator pos, std::initializer_list<value_type> ilist) {
+        return m_data.insert(pos, ilist);
     }
 
     void append(const ByteArray& other) {
-        data_.insert(data_.end(), other.begin(), other.end());
+        m_data.insert(m_data.end(), other.begin(), other.end());
     }
 
-    void append(const value_type* data, size_type size) {
-        data_.insert(data_.end(), data, data + size);
+    void append(const value_type* data, const size_type size) {
+        m_data.insert(m_data.end(), data, data + size);
     }
 
     void resize(size_type count) {
-        data_.resize(count);
+        m_data.resize(count);
     }
     void resize(size_type count, value_type value) {
-        data_.resize(count, value);
+        m_data.resize(count, value);
     }
 
     // Comparison operators
     bool operator==(const ByteArray& other) const {
-        return data_ == other.data_;
+        return m_data == other.m_data;
     }
 
     bool operator!=(const ByteArray& other) const {
@@ -129,7 +163,7 @@ class ByteArray {
         std::string result;
         result.reserve(size() * 2);
 
-        for (value_type byte : data_) {
+        for (value_type byte : m_data) {
             result.push_back(hex_chars[byte >> 4]);
             result.push_back(hex_chars[byte & 0x0F]);
         }
@@ -138,7 +172,7 @@ class ByteArray {
     }
 
     std::string string() const {
-        return std::string(reinterpret_cast<const char*>(data_.data()), data_.size());
+        return std::string(reinterpret_cast<const char*>(m_data.data()), m_data.size());
     }
 
     static ByteArray fromHexString(const std::string& hex) {
@@ -158,16 +192,16 @@ class ByteArray {
             }
 
             value_type byte = (hexCharToInt(high) << 4) | hexCharToInt(low);
-            result.data_.push_back(byte);
+            result.m_data.push_back(byte);
         }
 
         return result;
     }
 
   private:
-    std::vector<value_type> data_;
+    std::vector<value_type> m_data;
 
-    static value_type hexCharToInt(char c) {
+    static value_type hexCharToInt(const char c) {
         if (c >= '0' && c <= '9')
             return c - '0';
         if (c >= 'A' && c <= 'F')
