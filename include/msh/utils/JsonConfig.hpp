e7519a5b2a62926e22713855b7f14a602cca2f61
MShojaa <maziar.shojaa@gmail.com>
2025-05-23 23:55:46 +0330

    Add another method with default value inside

--- include/msh/utils/JsonConfig.hpp
+++ include/msh/utils/JsonConfig.hpp
@@ -13,13 +13,16 @@ template <typename T>
 inline T getSafe(const json& j, const std::string& key, const T& default_value);
 
 template <typename T>
+inline T getSafe(const json& j, const std::string& key);
+
+template <typename T>
 T getSafe(const json& j, const std::string& key, const T& default_value) {
     if (!j.contains(key)) {
         if constexpr (std::is_enum_v<T>) {
-            PLOG_WARNING << "doc[\"" << key << "\"] is not available & default value set: \""
+            PLOG_WARNING << "j[\"" << key << "\"] is not available & default value set: \""
                          << static_cast<std::underlying_type_t<T>>(default_value) << "\"";
         } else {
-            PLOG_WARNING << "doc[\"" << key << "\"] is not available & default value set: \""
+            PLOG_WARNING << "j[\"" << key << "\"] is not available & default value set: \""
                          << default_value << "\"";
         }
         return default_value;
@@ -29,12 +32,12 @@ T getSafe(const json& j, const std::string& key, const T& default_value) {
 
     if (jvalue.is_null()) {
         if constexpr (std::is_enum_v<T>) {
-            PLOG_WARNING << "doc[\"" << key << "\"] is null & default value set: \""
+            PLOG_WARNING << "j[\"" << key << "\"] is null & default value set: \""
                          << static_cast<std::underlying_type_t<T>>(default_value) << "\"";
             return default_value;
         } else {
-            PLOG_WARNING << "doc[\"" << key << "\"] is null & default value set: \""
-                         << default_value << "\"";
+            PLOG_WARNING << "j[\"" << key << "\"] is null & default value set: \"" << default_value
+                         << "\"";
             return default_value;
         }
     }
@@ -44,9 +47,8 @@ T getSafe(const json& j, const std::string& key, const T& default_value) {
             try {
                 return jvalue.get<T>();
             } catch (const std::exception& e) {
-                PLOG_WARNING << "doc[\"" << key.c_str()
-                             << "\"] enum conversion failed: " << e.what() << " | value: \""
-                             << jvalue.dump() << "\"";
+                PLOG_WARNING << "j[\"" << key.c_str() << "\"] enum conversion failed: " << e.what()
+                             << " | value: \"" << jvalue.dump() << "\"";
             }
             return default_value;
         }
@@ -74,9 +76,73 @@ T getSafe(const json& j, const std::string& key, const T& default_value) {
         }
     }
 
-    PLOG_WARNING << "doc[\"" << key << "\"] has invalid type: \"" << jvalue.type_name()
+    PLOG_WARNING << "j[\"" << key << "\"] has invalid type: \"" << jvalue.type_name()
                  << "\" & value: \"" << jvalue.dump() << "\"";
     return default_value;
+}
+
+template <typename T>
+T getSafe(const json& j, const std::string& key) {
+    if (!j.contains(key)) {
+        if constexpr (std::is_enum_v<T>) {
+            PLOG_WARNING << "j[\"" << key << "\"] is not available & default value set: \""
+                         << static_cast<std::underlying_type_t<T>>(T{}) << "\"";
+        } else {
+            PLOG_WARNING << "j[\"" << key << "\"] is not available & default value set: \"" << T{}
+                         << "\"";
+        }
+        return T{};
+    }
+
+    const auto& jvalue = j[key];
+
+    if (jvalue.is_null()) {
+        if constexpr (std::is_enum_v<T>) {
+            PLOG_WARNING << "j[\"" << key << "\"] is null & default value set: \""
+                         << static_cast<std::underlying_type_t<T>>(T{}) << "\"";
+        } else {
+            PLOG_WARNING << "j[\"" << key << "\"] is null & default value set: \"" << T{} << "\"";
+        }
+        return T{};
+    }
+
+    if constexpr (std::is_enum_v<T>) {
+        if (jvalue.is_string()) {
+            try {
+                return jvalue.get<T>();
+            } catch (const std::exception& e) {
+                PLOG_WARNING << "j[\"" << key.c_str() << "\"] enum conversion failed: " << e.what()
+                             << " | value: \"" << jvalue.dump() << "\"";
+            }
+            return T{};
+        }
+    } else if constexpr (std::is_same_v<T, bool>) {
+        if (jvalue.is_boolean()) {
+            return jvalue.get<T>();
+        }
+    } else if constexpr (std::is_integral_v<T>) {
+        if (jvalue.is_number_integer()) {
+            auto value = jvalue.get<int64_t>();
+            if (value >= static_cast<int64_t>(std::numeric_limits<T>::min()) &&
+                value <= static_cast<int64_t>(std::numeric_limits<T>::max())) {
+                return static_cast<T>(value);
+            } else {
+                return T{};
+            }
+        }
+    } else if constexpr (std::is_floating_point_v<T>) {
+        if (jvalue.is_number_float()) {
+            return jvalue.get<T>();
+        }
+    } else if constexpr (std::is_same_v<T, std::string>) {
+        if (jvalue.is_string()) {
+            return jvalue.get<T>();
+        }
+    }
+
+    PLOG_WARNING << "j[\"" << key << "\"] has invalid type: \"" << jvalue.type_name()
+                 << "\" & value: \"" << jvalue.dump() << "\"";
+    return T{};
 }
 }  // namespace JsonConfig
 
--- tests/json_config_test.cpp
+++ tests/json_config_test.cpp
@@ -6,14 +6,15 @@
 
 using namespace msh::utils;
 
-enum class MyEnum { Value1, Value2, Default };
+enum class MyEnum { Default, Value1, Value2 };
 NLOHMANN_JSON_SERIALIZE_ENUM(MyEnum, {
                                          {MyEnum::Default, "default"},
                                          {MyEnum::Value1, "value1"},
                                          {MyEnum::Value2, "value2"},
                                      })
 
-TEST_CASE("get value from json", "[json_config]") {
+TEST_CASE("T getSafe(const json& j, const std::string& key, const T& default_value)",
+          "[json_config]") {
     SECTION("int32_t") {
         nlohmann::json json_data = {{"int32_t", 42}, {"wrong_type", "value2"}};
         int32_t value = JsonConfig::getSafe(json_data, "int32_t", int32_t{});
@@ -25,17 +26,20 @@ TEST_CASE("get value from json", "[json_config]") {
     }
 
     SECTION("bool") {
-        nlohmann::json json_data = {{"bool", true}, {"wrong_type", "value2"}};
+        nlohmann::json json_data = {{"bool", true}, {"wrong_type", "value2"}, {"wrong_type2", 53}};
         bool value = JsonConfig::getSafe(json_data, "bool", bool{});
         REQUIRE(value == true);
         bool wrong_type_value = JsonConfig::getSafe(json_data, "wrong_type", bool{});
         REQUIRE(wrong_type_value == false);
+        bool wrong_type2_value = JsonConfig::getSafe(json_data, "wrong_type2", bool{});
+        REQUIRE(wrong_type2_value == false);
         bool missing_value = JsonConfig::getSafe(json_data, "missing_key", bool{});
         REQUIRE(missing_value == false);
     }
 
     SECTION("float") {
-        nlohmann::json json_data = {{"float", 3.14f}, {"wrong_type", "value2"}, {"wrong_type2", 12}};
+        nlohmann::json json_data = {
+            {"float", 3.14f}, {"wrong_type", "value2"}, {"wrong_type2", 12}};
         float value = JsonConfig::getSafe(json_data, "float", float{});
         REQUIRE(value == 3.14f);
         float wrong_type_value = JsonConfig::getSafe(json_data, "wrong_type", float{});
@@ -97,6 +101,97 @@ TEST_CASE("get value from json", "[json_config]") {
         int8_t wrong_type2_value = JsonConfig::getSafe(json_data, "wrong_type2", int8_t{});
         REQUIRE(wrong_type2_value == 0);
         int8_t missing_value = JsonConfig::getSafe(json_data, "missing_key", int8_t{});
+        REQUIRE(missing_value == 0);
+    }
+}
+
+TEST_CASE("T getSafe(const json& j, const std::string& key)", "[json_config]") {
+    SECTION("int32_t") {
+        nlohmann::json json_data = {{"int32_t", 42}, {"wrong_type", "value2"}};
+        int32_t value = JsonConfig::getSafe<int32_t>(json_data, "int32_t");
+        REQUIRE(value == 42);
+        int32_t wrong_type_value = JsonConfig::getSafe<int32_t>(json_data, "wrong_type");
+        REQUIRE(wrong_type_value == 0);
+        int32_t missing_value = JsonConfig::getSafe<int32_t>(json_data, "missing_key");
+        REQUIRE(missing_value == 0);
+    }
+
+    SECTION("bool") {
+        nlohmann::json json_data = {{"bool", true}, {"wrong_type", "value2"}, {"wrong_type2", 53}};
+        bool value = JsonConfig::getSafe<bool>(json_data, "bool");
+        REQUIRE(value == true);
+        bool wrong_type_value = JsonConfig::getSafe<bool>(json_data, "wrong_type");
+        REQUIRE(wrong_type_value == false);
+        bool wrong_type2_value = JsonConfig::getSafe<bool>(json_data, "wrong_type2");
+        REQUIRE(wrong_type2_value == false);
+        bool missing_value = JsonConfig::getSafe<bool>(json_data, "missing_key");
+        REQUIRE(missing_value == false);
+    }
+
+    SECTION("float") {
+        nlohmann::json json_data = {
+            {"float", 3.14f}, {"wrong_type", "value2"}, {"wrong_type2", 12}};
+        float value = JsonConfig::getSafe<float>(json_data, "float");
+        REQUIRE(value == 3.14f);
+        float wrong_type_value = JsonConfig::getSafe<float>(json_data, "wrong_type");
+        REQUIRE(wrong_type_value == 0.0f);
+        float wrong_type2_value = JsonConfig::getSafe<float>(json_data, "wrong_type2");
+        REQUIRE(wrong_type2_value == 0.0f);
+        float missing_value = JsonConfig::getSafe<float>(json_data, "missing_key");
+        REQUIRE(missing_value == 0.0f);
+    }
+
+    SECTION("double") {
+        nlohmann::json json_data = {{"double", 3.14}, {"wrong_type", "value2"}};
+        double value = JsonConfig::getSafe<double>(json_data, "double");
+        REQUIRE(value == 3.14);
+        double wrong_type_value = JsonConfig::getSafe<double>(json_data, "wrong_type");
+        REQUIRE(wrong_type_value == 0.0);
+        double missing_value = JsonConfig::getSafe<double>(json_data, "missing_key");
+        REQUIRE(missing_value == 0.0);
+    }
+
+    SECTION("string") {
+        nlohmann::json json_data = {{"string", "value1"}, {"wrong_type", 42}};
+        std::string value = JsonConfig::getSafe<std::string>(json_data, "string");
+        REQUIRE(value == "value1");
+        std::string wrong_type_value = JsonConfig::getSafe<std::string>(json_data, "wrong_type");
+        REQUIRE(wrong_type_value == "");
+        std::string missing_value = JsonConfig::getSafe<std::string>(json_data, "missing_key");
+        REQUIRE(missing_value == "");
+    }
+
+    SECTION("enum") {
+        nlohmann::json json_data = {{"enum", "value1"}, {"wrong_type", 42}};
+        MyEnum value = JsonConfig::getSafe<MyEnum>(json_data, "enum");
+        REQUIRE(value == MyEnum::Value1);
+        MyEnum wrong_type_value = JsonConfig::getSafe<MyEnum>(json_data, "wrong_type");
+        REQUIRE(wrong_type_value == MyEnum::Default);
+        MyEnum missing_value = JsonConfig::getSafe<MyEnum>(json_data, "missing_key");
+        REQUIRE(missing_value == MyEnum::Default);
+    }
+
+    SECTION("uint8_t") {
+        nlohmann::json json_data = {{"uint8_t", 42}, {"wrong_type", -12}, {"wrong_type2", 300}};
+        uint8_t value = JsonConfig::getSafe<uint8_t>(json_data, "uint8_t");
+        REQUIRE(value == 42);
+        uint8_t wrong_type_value = JsonConfig::getSafe<uint8_t>(json_data, "wrong_type");
+        REQUIRE(wrong_type_value == 0);
+        uint8_t wrong_type2_value = JsonConfig::getSafe<uint8_t>(json_data, "wrong_type2");
+        REQUIRE(wrong_type2_value == 0);
+        uint8_t missing_value = JsonConfig::getSafe<uint8_t>(json_data, "missing_key");
+        REQUIRE(missing_value == 0);
+    }
+
+    SECTION("int8_t") {
+        nlohmann::json json_data = {{"int8_t", -12}, {"wrong_type", 300}, {"wrong_type2", -400}};
+        int8_t value = JsonConfig::getSafe<int8_t>(json_data, "int8_t", int8_t{});
+        REQUIRE(value == -12);
+        int8_t wrong_type_value = JsonConfig::getSafe<int8_t>(json_data, "wrong_type");
+        REQUIRE(wrong_type_value == 0);
+        int8_t wrong_type2_value = JsonConfig::getSafe<int8_t>(json_data, "wrong_type2");
+        REQUIRE(wrong_type2_value == 0);
+        int8_t missing_value = JsonConfig::getSafe<int8_t>(json_data, "missing_key");
         REQUIRE(missing_value == 0);
     }
 }
